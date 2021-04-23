569e9388df52080064cb39c66ef3bf70b3724b4d
xzhangkellogg <81667306+xzhangkellogg@users.noreply.github.com>
2021-04-22 21:44:55 -0500

    Initial commit

---
+++ noober.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', async function() {
+  let response = await fetch('https://kiei451.com/api/rides.json')
+  let json = await response.json()
+
+  // writes the returned JSON to the console
+  console.dir(json)
+  
+  // 🔥 start here: write the recipe (algorithm), then write the code
+  
+})
